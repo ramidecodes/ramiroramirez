f10f02e5bc29faaae2593d10c9e2095239fc398a
ecovirtual <ramiro.ecovirtual@gmail.com>
2019-07-24 19:18:52 +0200

    Update SEO

--- src/components/common/SEO/index.jsx
+++ src/components/common/SEO/index.jsx
@@ -70,6 +70,26 @@ export const SEO = ({
 			<meta name="twitter:description" content={description} />
 			<meta name="twitter:image:src" content={Thumbnail} />
 			<script type="application/ld+json">{structuredDataOrganization}</script>
+
+			<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+			<link rel="manifest" href="/favicon/site.webmanifest" />
+			<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#a432ce" />
+			<link rel="shortcut icon" href="/favicon/favicon.ico" />
+			<meta name="msapplication-TileColor" content="#00aba9" />
+			<meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+			<meta name="theme-color" content="#ffffff" />
+
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link rel="manifest" href="/site.webmanifest" />
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+			<meta name="msapplication-TileColor" content="#da532c" />
+			<meta name="theme-color" content="#ffffff" />
+
+
 			<title>{title}</title>
 			<html lang="en" dir="ltr" />
 		</Helmet>
