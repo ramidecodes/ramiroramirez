7bbf73197d9e8d048ce960136b36b7e0cd04fbbc
ecovirtual <ramiro.ecovirtual@gmail.com>
2019-07-24 19:18:34 +0200

    Update Skills section

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Container, Button } from 'Common'
-import dev from 'Static/illustrations/skills.svg'
-import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles'
+// import campfire from 'Static/illustrations/campfire.svg'
+import { Wrapper, SkillsWrapper, SkillsColumn } from './styles'
 
 export const Skills = () => (
-	<Wrapper id="about">
+	<Wrapper id="skills">
+		<h2>How can I help you?</h2>
 		<SkillsWrapper as={Container}>
-			<Thumbnail>
-				<img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
-			</Thumbnail>
-			<Details>
-				<h1>Hi There!</h1>
-				<p>
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry’s standard dummy.
-				</p>
-				<Button as={AnchorLink} href="#contact">
-					Hire me
-				</Button>
-			</Details>
+			<SkillsColumn>
+				<p>As a <strong>software developer</strong> I can deliver complete projects in the shape of:</p>
+				<ul>
+					<li>Websites</li>
+					<li>Web Apps</li>
+					<li>Desktop Apps</li>
+					<li>Docker Images (APIs & Microservices)</li>
+					<li>Data Scraping Tools</li>
+					<li>Data visualization dashboards</li>
+				</ul>
+			</SkillsColumn>
+			<SkillsColumn>
+				<p>As a <strong>consultant</strong> I can help you and your team:</p>
+				<ul>
+					<li>Adopt best practices for agile software development & team management</li>
+					<li>Transition into a data driven decision-making culture</li>
+					<li>Design Big Data warehousing & analysis pipelines</li>
+					<li>Asses the technical feasibility & scalability of an idea or prototype</li>
+				</ul>
+			</SkillsColumn>
 		</SkillsWrapper>
 	</Wrapper>
 )
