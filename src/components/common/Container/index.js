00b4efdc395ad4f15e27b1c883a4ad0e69c7ae9c
ecovirtual <ramiro.ecovirtual@gmail.com>
2019-07-26 19:14:41 +0200

    Update layout margins

--- src/components/common/Container/index.js
+++ src/components/common/Container/index.js
@@ -2,14 +2,16 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
 	max-width: 1280px;
-	margin: 0 auto;
+	margin: 0 auto 5rem;
 	width: 90%;
-
 	@media (min-width: 601px) {
 		width: 90%;
 	}
 
 	@media (min-width: 993px) {
 		width: 80%;
+	}
+	@media (max-width: 993px) {
+		margin: 0 auto;
 	}
 `
