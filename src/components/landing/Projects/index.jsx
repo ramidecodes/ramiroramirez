a1ea9a55f47707d13fc1a8fb6f76f7d815e0f9bd
ecovirtual <ramiro.ecovirtual@gmail.com>
2019-07-26 19:15:03 +0200

    Update projects title to Previous Work

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -39,7 +39,7 @@ export const Projects = () => {
 	`)
 	return (
 		<Wrapper as={Container} id="projects">
-			<h2>Projects</h2>
+			<h2>Recent Work</h2>
 			<Grid>
 				{edges.map(({ node }) => (
 					<Item
