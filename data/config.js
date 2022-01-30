ce0ba08775ee881b367c4b1904a580490a963e49
Ramiro Ramirez <ram@foryouandyourcustomers.com>
2022-01-30 12:14:56 +0100

    Update contact email

--- data/config.js
+++ data/config.js
@@ -1,33 +1,33 @@
 module.exports = {
-	defaultTitle: 'Ramiro Ramirez - Creative Technologist & Software Developer',
-	logo: 'https://ramiroramirez.xyz/favicon/android-chrome-512x512.png',
-	author: 'Ramiro Ramirez',
-	url: 'https://ramiroramirez.xyz',
-	legalName: 'Ramiro Ramirez',
-	defaultDescription: 'Hi, I’m Ramiro. Let’s talk about humans and software.',
-	socialLinks: {
-		twitter: 'http://www.twitter.com/eco_virtual',
-		github: 'https://github.com/ecovirtual',
-		linkedin: 'https://www.linkedin.com/in/ramirezramiro/',
-		instagram: 'https://instagram.com/ecovirtual'
-	},
-	googleAnalyticsID: 'UA-144485551-1',
-	themeColor: '#9919C9',
-	backgroundColor: '#9919C9',
-	social: {
-		facebook: '642851369553020',
-		twitter: '@eco_virtual',
-	},
-	address: {
-		city: 'Amsterdam',
-		region: 'Noord Holland',
-		country: 'The Netherlands',
-		zipCode: '1021JE',
-	},
-	contact: {
-		email: 'hello@ramiroramirez.xyz',
-		phone: 'xxxxxxxx',
-	},
-	foundingDate: '2012',
-	recaptcha_key: '6LdVJbEUAAAAAHXDwVQ-a1CK86dpMPi4HwLaqyif'
+  defaultTitle: 'Ramiro Ramirez - Creative Technologist & Software Developer',
+  logo: 'https://ramiroramirez.xyz/favicon/android-chrome-512x512.png',
+  author: 'Ramiro Ramirez',
+  url: 'https://ramiroramirez.xyz',
+  legalName: 'Ramiro Ramirez',
+  defaultDescription: 'Hi, I’m Ramiro. Let’s talk about humans and software.',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/eco_virtual',
+    github: 'https://github.com/ecovirtual',
+    linkedin: 'https://www.linkedin.com/in/ramirezramiro/',
+    instagram: 'https://instagram.com/ecovirtual',
+  },
+  googleAnalyticsID: 'UA-144485551-1',
+  themeColor: '#9919C9',
+  backgroundColor: '#9919C9',
+  social: {
+    facebook: '642851369553020',
+    twitter: '@eco_virtual',
+  },
+  address: {
+    city: 'Amsterdam',
+    region: 'Noord Holland',
+    country: 'The Netherlands',
+    zipCode: '1021JE',
+  },
+  contact: {
+    email: 'ramiro.ecovirtual@gmail.com',
+    phone: 'xxxxxxxx',
+  },
+  foundingDate: '2012',
+  recaptcha_key: '6LdVJbEUAAAAAHXDwVQ-a1CK86dpMPi4HwLaqyif',
 }
